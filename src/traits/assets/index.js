17b3914c56a82f3cac6666f03ebda6b1ef1e2c88
Ashelynne Juniper <ashe.juniper@pm.me>
2023-12-17 07:13:29 -0600

    Create index.js

---
+++ src/traits/assets/index.js
@@ -0,0 +1,11 @@
+const Asset = require('./Asset');
+const AssetLoader = require('./AssetLoader');
+const Sound = require('./Sound');
+const Image = require('./Image');
+
+module.exports = {
+    Asset,
+    AssetLoader,
+    Sound,
+    Image
+};
