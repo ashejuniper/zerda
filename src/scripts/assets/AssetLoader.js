e8fa976f23143459ca1bbc65cc92d9ff8a16c38e
Ashelynne Juniper <ashe.juniper@pm.me>
2023-12-19 02:10:33 -0600

    Update AssetLoader.js

--- src/scripts/assets/AssetLoader.js
+++ src/scripts/assets/AssetLoader.js
@@ -1,15 +1,18 @@
 const Asset = require("./Asset");
 const Sound = require("./Sound");
 const Image = require("./Image");
+const ScriptVM = require("../ScriptVM");
 
 class AssetLoader {
     static async loadAll () {
         await Sound._loadAll();
         await Image._loadAll();
+
+        await ScriptVM.main.loadAllModules();
     }
 
     static async unloadAll () {
-        Asset._unloadAll();
+        await Asset._unloadAll();
     }
 }
 
