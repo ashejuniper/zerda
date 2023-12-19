cf0cebaa1b7f6f6e3f4a1a6af6c9467eb4e9ddfe
Ashelynne Juniper <ashe.juniper@pm.me>
2023-12-19 02:10:24 -0600

    Update Asset.js

--- src/scripts/assets/Asset.js
+++ src/scripts/assets/Asset.js
@@ -1,18 +1,41 @@
-const glob = require('glob');
+const path = require('path');
+const { glob } = require('glob');
 const Script = require('../Script');
 
 const assets = {};
 
-function getVirtualPath (asset) {
-    return `assets://${asset.modId}/${asset.constructor.name}/${asset.assetId}`;
+function removeDuplicates (a) {
+    var seen = {};
+
+    return a.filter(
+        function(item) {
+            return seen.hasOwnProperty(item) ? false : (seen[item] = true);
+        }
+    );
 }
 
-function addAsset (asset) {
-    assets[getVirtualPath(asset)] = asset;
+function createVirtualPath (modId, assetId, assetTypeName) {
+    return `assets://${modId}/${assetTypeName}/${assetId}`;
+}
+
+function getVirtualPath (asset, assetTypeName = null) {
+    if (typeof assetTypeName !== 'string') {
+        assetTypeName = asset.constructor.name;
+    }
+
+    return createVirtualPath(
+        asset.modId,
+        asset.assetId,
+        assetTypeName
+    );
+}
+
+function addAsset (asset, assetTypeName = null) {
+    assets[getVirtualPath(asset, assetTypeName)] = asset;
 }
 
 function getAsset (virtualPath) {
-    return assets[virtualPath];
+    return assets[virtualPath] || null;
 }
 
 function removeAsset (asset) {
@@ -22,29 +45,133 @@ function removeAsset (asset) {
 }
 
 class Asset extends Script {
-    constructor (entity = null) {
-        super(entity);
+    constructor () {
+        super();
 
         this._isRegistered = false;
+    }
+
+    static async getAssetPaths (
+        modDirectory,
+        assetTypeName,
+        ...fileExtensions
+    ) {
+        const assetPaths = [];
+
+        for (const extension of fileExtensions) {
+            assetPaths.push(
+                ...(await glob(`${modDirectory}/${assetTypeName}/*.${extension}`)),
+                ...(await glob(`${modDirectory}/${assetTypeName}/**/*.${extension}`))
+            );
+        }
+
+        return removeDuplicates(assetPaths);
+    }
+
+    static async getModDirectories () {
+        return await glob("assets/*/");
+    }
+
+    static async forEachModDirectory (cb) {
+        const modDirectories = await this.getModDirectories();
+
+        for (const modDirectory of modDirectories) {
+            const modName = path.basename(modDirectory);
+
+            await cb(
+                {
+                    modDirectory,
+                    modName
+                }
+            );
+        }
+    }
+
+    static async forEachFile (assetTypeName, fileExtensions, cb) {
+        await this.forEachModDirectory(
+            async (e) => {
+                console.info(`INFO: ASSETLOADER:   Loading images for mod '${e.modName}'...`);
+
+                if (e.modName !== assetTypeName) {
+                    const assetPaths = await Asset.getAssetPaths(
+                        e.modDirectory,
+                        assetTypeName,
+                        ...fileExtensions
+                    );
+
+                    for (const assetPath of assetPaths) {
+                        let assetId = path.basename(assetPath);
+
+                        for (const extension of fileExtensions) {
+                            assetId = assetId.replaceAll(`.${extension}`, '');
+                        }
+
+                        console.info(`INFO: ASSETLOADER:     Loading image "assets://${e.modName}/Image/${assetId}"...`);
+
+                        const modName = e.modName;
+
+                        void cb(
+                            {
+                                assetId,
+                                assetPath,
+                                modName
+                            }
+                        );
+                    }
+                }
+            }
+        )
     }
 
     static get (modName, assetId, assetType) {
         let virtualPath;
 
-        if (!assetId) {
+        if (typeof assetId !== 'string') {
             virtualPath = modName;
         } else {
-            virtualPath = `${modName}/${assetId}.${assetType}`;
+            virtualPath = createVirtualPath(modName, assetId, assetType);
         }
 
         return getAsset(virtualPath);
+    }
+
+    static register (
+        asset,
+        modId,
+        assetId,
+        assetTypeName = null
+    ) {
+        if (
+            !asset
+                || (
+                    typeof asset !== 'object'
+                        && typeof asset !== 'function'
+                )
+        ) {
+            return;
+        }
+
+        asset.modId = modId;
+        asset.assetId = assetId;
+
+        addAsset(asset, assetTypeName);
+
+        asset._isRegistered = true;
+    }
+
+    static unregister (asset) {
+        removeAsset(asset);
+
+        asset._isRegistered = false;
     }
 
     static async _unloadAll () {
         for (const id in assets) {
             const asset = assets[id];
 
-            await asset.disable();
+            if (asset.isEnabled()) {
+                await asset.onDisable();
+            }
         }
     }
 
@@ -53,42 +180,35 @@ class Asset extends Script {
     }
 
     register (modId, assetId) {
-        this.modId = modId;
-        this.assetId = assetId;
-
-        addAsset(this);
-
-        this._isRegistered = true;
+        Asset.register(this, modId, assetId);
     }
 
     unregister () {
-        removeAsset(this);
-
-        this._isRegistered = false;
+        Asset.unregister(this);
     }
 
     virtualPath () {
         return getVirtualPath(this);
     }
 
-    async disable (e) {
-        super.disable(e);
+    async onDisable (e) {
+        super.onDisable(e);
 
         if (this.isRegistered()) {
             this.unregister();
         }
 
-        console.log(`Image "${this.virtualPath()}" disabled`);
+        console.info(`INFO: ASSET: [${this.virtualPath()}] Asset disabled`);
     }
 
-    async enable (e) {
-        super.enable(e);
+    async onEnable (e) {
+        super.onEnable(e);
 
         if (!this.isRegistered()) {
             this.register(this.modId, this.assetId);
         }
 
-        console.log(`Image "${this.virtualPath()}" enabled`);
+        console.info(`INFO: ASSET: [${this.virtualPath()}] Asset enabled`);
     }
 }
 
