e0527a34ae7a8d4d3449b49b24253922933176a8
Ashelynne Juniper <ashe.juniper@pm.me>
2023-12-19 02:10:42 -0600

    Update EntityStatus.js

--- src/entities/EntityStatus.js
+++ src/entities/EntityStatus.js
@@ -2,12 +2,11 @@ class EntityStatus extends Number {}
 
 EntityStatus.ERROR = new EntityStatus(0);
 EntityStatus.CREATE = new EntityStatus(1);
-EntityStatus.CREATED = new EntityStatus(2);
-EntityStatus.ENABLED = new EntityStatus(3);
-EntityStatus.DESTROY = new EntityStatus(4);
-EntityStatus.DESTROYED = new EntityStatus(5);
-EntityStatus.ENABLE = new EntityStatus(6);
-EntityStatus.DISABLE = new EntityStatus(7);
-EntityStatus.DISABLED = new EntityStatus(8);
+EntityStatus.ENABLED = new EntityStatus(2);
+EntityStatus.DESTROY = new EntityStatus(3);
+EntityStatus.DESTROYED = new EntityStatus(4);
+EntityStatus.ENABLE = new EntityStatus(5);
+EntityStatus.DISABLE = new EntityStatus(6);
+EntityStatus.DISABLED = new EntityStatus(7);
 
 module.exports = EntityStatus;
